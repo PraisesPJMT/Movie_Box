c4bb2c9f02fc4b3d6cd0c14f6fcea6650733ec9a
praisespjmt <praisesmusa@outlook.com>
2023-09-14 09:17:29 +0100

    setup app store

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,74 @@
 // Application Store
+
+import { create } from 'zustand';
+import { API_STATUS } from '../utilities/enums';
+import API from './api';
+
+const useMovieStore = create((set) => ({
+  // Search State
+  search: {
+    data: null,
+    status: API_STATUS.IDLE,
+  },
+
+  //   Movies State
+  movies: {
+    data: null,
+    status: API_STATUS.IDLE,
+  },
+
+  //   Movie State
+  movie: {
+    data: null,
+    status: API_STATUS.IDLE,
+  },
+
+  //   Get Movie
+  getMovie: (movieId) => {},
+
+  //   Get Movies
+  getMovies: async () => {
+    set((state) => ({
+      ...state,
+      movies: {
+        ...state.movies,
+        status: API_STATUS.LOADING,
+      },
+    }));
+
+    const { data, status } = await API.getMovies();
+
+    set((state) => ({
+      ...state,
+      movies: {
+        ...state.movies,
+        status,
+        data: data ? data.slice(0, 10) : data,
+      },
+    }));
+  },
+
+  //   Search for movies
+  searchMovies: async (query) => {
+    set((state) => ({
+      ...state,
+      search: {
+        ...state.search,
+        status: API_STATUS.LOADING,
+      },
+    }));
+
+    const { data, status } = await API.searchMovie(query);
+
+    set((state) => ({
+      ...state,
+      search: {
+        ...state.search,
+        status,
+        data,
+      },
+    }));
+  },
+}));
+
+export default useMovieStore;
